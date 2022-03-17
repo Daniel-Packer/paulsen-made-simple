7d603826e2cebbd7e7c5473fe938e2355da599be
Daniel-Packer <62404584+Daniel-Packer@users.noreply.github.com>
2022-03-17 16:32:26 -0400

    progress on first bound in paulsen

--- src/perturbation.lean
+++ src/perturbation.lean
@@ -2,6 +2,7 @@ import data.real.basic
 import linear_algebra.basic
 import linear_algebra.linear_independent
 import analysis.normed_space.basic
+import analysis.inner_product_space.euclidean_dist
 
 
 open_locale big_operators
@@ -20,18 +21,18 @@ variables [has_norm M]
 -- variable [finite_dimensional.finrank ℝ M = d] <- Want to include this somehow
 
 -- Warm up? (Not necessary)
-def perturbations' (ε : ℝ) (u : (fin d) → M) : fin d → M := sorry
+def perturbations' (ε : ℝ) (u : (fin d) → (fin d) → ℝ) : fin d → fin d → ℝ := sorry
 
-lemma perturbations'_apply (ε : ℝ) (u : (fin d) → M) : linear_independent ℝ (λ i : fin d, u i + perturbations' ε u i) := sorry
+lemma perturbations'_apply (ε : ℝ) (u : (fin d) → (fin d) → ℝ) : linear_independent ℝ (λ i : fin d, u i + perturbations' ε u i) := sorry
 
-lemma perturbations'_bound (ε : ℝ) (u : (fin d) → M) : ∀ i : fin d, ∥ perturbations' ε u i ∥^2 ≤ ε := sorry
+lemma perturbations'_bound (ε : ℝ) (u : (fin d) → fin d → ℝ) : ∀ i : fin d, ∥ to_euclidean (perturbations' ε u i) ∥^2 ≤ ε := sorry
 
 
 --What we actually need:
-def perturbations (ε : ℝ) (u : (fin n) → M) : fin n → M := sorry
+def perturbations (ε : ℝ) (u : (fin n) → fin d → ℝ) : fin n → fin d → ℝ := sorry
 
 /-- The perturbations make -/
-lemma perturbations_apply (ε : ℝ) (u : (fin n) → M) {f : (fin d) → (fin n)} (hf : function.injective f) : 
+lemma perturbations_apply (ε : ℝ) (u : (fin n) → fin d → ℝ) {f : (fin d) → (fin n)} (hf : function.injective f) : 
   linear_independent ℝ (λ i : fin d, u (f i) + perturbations ε u (f i)) := sorry
 
-lemma perturbations_bound (ε : ℝ) (u : (fin n) → M) : ∀ i : fin n, ∥ perturbations ε u i ∥^2 ≤ ε := sorry
+lemma perturbations_bound (ε : ℝ) (u : (fin n) → fin d → ℝ) : ∀ i : fin n, ∥ to_euclidean (perturbations ε u i) ∥^2 ≤ ε := sorry
